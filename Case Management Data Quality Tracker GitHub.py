26cd89f4f76f79e2a65f586fa9a4195c9741ea41
erwinma1 <erwin.ma@gmail.com>
2026-03-05 11:53:48 -0500

    Refactor data quality tracker with updated paths and formatting
    Updated file paths and added data formatting for appearance, matter, and initial charge data. Included additional comments and improved error handling.

--- Case Management Data Quality Tracker GitHub.py
+++ Case Management Data Quality Tracker GitHub.py
@@ -1,10 +1,16 @@
 '''
 Litigation Support Data Quality Tracker
 In order to keep track of LM data error validation progress, the Data Quality Tracker ingests data from LM and creates
-summary statistics and charts from the Data Quality Team
+summary statistics and charts from the Data Quality Team.
+
+Data collection began in Nov 2025. Previous dates are undercounted due to updated_by fields being overwritten resulting
+in data loss.
+
 1) Extract the data (monthly) from SQL into the directory folder
 2) Save a new file name
 3) Run the script
+
+For any questions contact ema@legal-aid.org
 '''
 
 
@@ -12,8 +18,10 @@ import os
 import pandas as pd
 import openpyxl as ox
 
+#Update file paths
 
-directory = 'C:/directory_file_path'
+directory = 'C:/Users/ema/OneDrive - The Legal Aid Society/Documents/SQL Server Management Studio/LM Data Quality Tracker/Archive'
+write_path = 'C:/Users/ema/OneDrive - The Legal Aid Society/Documents/PycharmProjects LAS/Litigation Support Data Quality Tracker/Data Quality Dashboard 3.3.26.xlsx'
 
 data_frames_appear = []
 data_frames_matter = []
@@ -40,14 +48,57 @@ for file in os.listdir(directory):
         except Exception as e:
             print(f"Skipping {file}: e")
 
-#Format Case Appearance Data
+#------------------Format Case Appearance Data------------------#
 data_appear = pd.concat(data_frames_appear)
-data_appear['date_updated'] = pd.to_datetime(data_appear['date_updated'], format='%m/%d/%Y %H:%M') #date updated is the unique id
+data_appear['date_updated'] = pd.to_datetime(data_appear['date_updated'], format='%m/%d/%Y %H:%M', errors='coerce') #date updated + updated by are the unique ids
 data_appear = data_appear.sort_values(by='date_updated', ascending=False)
+#remove dupes
 data_appear = data_appear.drop_duplicates(subset=['date_updated','updated_by'])
 
-data_appear.to_excel('C:/Project_Write_Path/Project.xlsx', index=False)
+pivot_appear = data_appear
 
-#data_matter = pd.concat(data_frames_appear)
+#format date
+pivot_appear['update_date'] = pivot_appear['date_updated'].dt.to_period('M').dt.to_timestamp()
 
-#data_init_charge = pd.concat(data_frames_appear)
+#drop columns
+pivot_appear = pivot_appear.drop(columns=['date_updated','updated_by'])
+
+pivot_appear = pd.pivot_table(data=pivot_appear, values='appear_count', index='update_date', aggfunc='sum')
+pivot_appear = pivot_appear.reset_index()
+
+#-------------------------Format Matter Data--------------------------#
+data_matter = pd.concat(data_frames_matter)
+
+data_matter['date_updated'] = pd.to_datetime(data_matter['date_updated'], errors='coerce')
+data_matter['update_date'] = data_matter['date_updated'].dt.to_period('M').dt.to_timestamp()
+data_matter = data_matter.drop_duplicates(subset=['date_updated','updated_by'])
+
+
+pivot_matter = data_matter.drop(columns=['date_updated','updated_by'])
+
+pivot_matter = pd.pivot_table(data=pivot_matter, values='matter_count', index='update_date', aggfunc='sum')
+
+pivot_matter = pivot_matter.reset_index()
+
+#-------------------------Format Init Top Charge Data--------------------------#
+data_intcharge = pd.concat(data_frames_init_charge)
+
+data_intcharge['date_added'] = pd.to_datetime(data_intcharge['date_added'], errors='coerce')
+data_intcharge['added_date'] = data_intcharge['date_added'].dt.to_period('M').dt.to_timestamp()
+data_intcharge = data_intcharge.drop_duplicates(subset=['date_added', 'added_by'])
+
+
+pivot_intcharge = data_intcharge
+
+pivot_intcharge = data_intcharge.drop(columns=['charge_key', 'added_by', 'date_added'])
+
+pivot_intcharge = pd.pivot_table(data=pivot_intcharge, values='matter_key', index='added_date', aggfunc='count')
+
+pivot_intcharge = pivot_intcharge.reset_index()
+
+#------------------------- Write File --------------------------#
+
+with pd.ExcelWriter(write_path, engine='openpyxl') as writer:
+    pivot_appear.to_excel(writer, sheet_name='Case_Appearance', index=False)
+    pivot_matter.to_excel(writer, sheet_name='Matter', index=False)
+    pivot_intcharge.to_excel(writer, sheet_name='Init_Top_Charge', index=False)
